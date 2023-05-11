37c83e11c819612ecdeef60a4a07a862450419b2
gaudiosi <g.gaudiosi1@studenti.unisa.it>
2023-05-11 22:00:12 +0200

    duplicazione, commenti e css

--- SpaceComix/src/it/SpaceComix/control/SigninController.java
+++ SpaceComix/src/it/SpaceComix/control/SigninController.java
@@ -13,33 +13,22 @@ import javax.servlet.http.HttpSession;
 import it.SpaceComix.model.UserBean;
 import it.SpaceComix.model.UserDAO;
 
-/**
- * Servlet implementation class SigninController
- */
 @WebServlet("/Signin")
 public class SigninController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SigninController() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		boolean errore = false; 
+		HttpSession session = request.getSession();
 		UserBean user = new UserBean();
         user.setEmail(request.getParameter("email"));
         user.setPassword(request.getParameter("password1"));
@@ -58,16 +47,14 @@ public class SigninController extends HttpServlet {
 			} catch (SQLException e) {
 				errore = true;
 			}
-			if(errore == false) {
-				HttpSession session = request.getSession();
+			if(!errore) {
             	session.setAttribute("user", user);
             	response.sendRedirect("index.jsp");
             }
 		}else {
-		String error = "Invalid paramiter. Please try again.";
-        HttpSession session = request.getSession();
-        session.setAttribute("error", error);
-        response.sendRedirect("register.jsp");
+			String error = "Invalid paramiter. Please try again.";
+			session.setAttribute("error", error);
+			response.sendRedirect("register.jsp");
 		}
     }
 }
